0960e8883c2c9ef4e547f220de4b82e20d438bfe
SPENCERSSURFACE\spenc <spencercdoughty@gmail.com>
2018-10-23 18:22:55 -0400

    First Code

---
+++ src/org/frc5687/westcoast/robot/subsystems/DriveTrain.java
@@ -0,0 +1,11 @@
+package org.frc5687.westcoast.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class DriveTrain extends Subsystem {
+
+    @Override
+    protected void initDefaultCommand() {
+
+    }
+}
