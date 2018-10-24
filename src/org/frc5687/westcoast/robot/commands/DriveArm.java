2ade70d989d6faf955e4d982d4c6348acb101cfa
Nathan Desjardins <Nathan.desjardins@baxter-academy.org>
2018-10-24 19:00:06 -0400

    Initial commit for arm and armdrive

---
+++ src/org/frc5687/westcoast/robot/commands/DriveArm.java
@@ -0,0 +1,15 @@
+package org.frc5687.westcoast.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class DriveArm extends Command {
+    
+    public DriveArm(){
+
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+}
