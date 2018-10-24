6fdbfebc06592363b41345c64ef4dca0b4edfdfa
Nathan Desjardins <Nathan.desjardins@baxter-academy.org>
2018-10-24 19:23:46 -0400

    commit to alter master

--- src/org/frc5687/westcoast/robot/commands/DriveArm.java
+++ src/org/frc5687/westcoast/robot/commands/DriveArm.java
@@ -1,12 +1,19 @@
 package org.frc5687.westcoast.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import org.frc5687.westcoast.robot.OI;
+import org.frc5687.westcoast.robot.subsystems.Arm;
 
 public class DriveArm extends Command {
-    
-    public DriveArm(){
-
+    private OI _oi;
+    private Arm _arm;
+    public DriveArm(OI oi, Arm arm){
+        _oi = oi;
+        _arm = arm;
+        requires(arm);
     }
+    @Override
+    protected  void execute(){}
 
     @Override
     protected boolean isFinished() {
--- src/org/frc5687/westcoast/robot/subsystems/Arm.java
+++ src/org/frc5687/westcoast/robot/subsystems/Arm.java
@@ -1,11 +1,20 @@
 package org.frc5687.westcoast.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import org.frc5687.westcoast.robot.Robot;
+import edu.wpi.first.wpilibj.VictorSP;
+
 
 public class Arm extends Subsystem {
-
+    private Robot _robot;
+    private VictorSP _motor;
     @Override
     protected void initDefaultCommand() {
 
+    }
+    public Arm(Robot robot) {
+
+        _robot = robot;
+        _motor = new VictorSP(RobotMap.PWM.ARM_MOTOR);
     }
 }
